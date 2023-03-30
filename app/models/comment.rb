d4329625f793b746412ebae5ae8b6d28990269d4
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-30 16:24:54 -0500

    Create custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def self.update_counter(post_id, value)
+    post = Post.where(id: post_id)
+    post.update(comments_counter: value)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def self.update_counter(user_id, value)
+    user = User.where(id: user_id)
+    user.update(posts_counter: value)
+  end
+
+  def self.get_recent_comments(post_id)
+    Comment.where(post_id: post_id).order(created_at: :desc).limit(5)
+  end
 end
