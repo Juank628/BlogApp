e01df3c896d24664ba80572c71170874002505d7
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-04 13:56:49 -0500

    add actions to controllers and correct route issue

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #root "users#index"
   
   get "/users/:id/posts", to: "posts#index"
-  get "/users/:id/posts/:id", to: "posts#show"
+  get "/users/:user_id/posts/:id", to: "posts#show"
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
 
