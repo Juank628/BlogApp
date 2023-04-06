eeb6e442789a463d289af3dcb3d5debc84d31706
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-06 11:45:19 -0500

    Add data to posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    user_id = request.params['id'].to_i
+    @user = User.find(user_id)
+    @posts_list = @user.recent_posts
+  end
 
   def show; end
 end
