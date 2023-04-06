c460e2b106c80867a6d6535ef4fe52fed651e7a9
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-06 14:39:30 -0500

    show the coorect number of posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     user_id = request.params['id'].to_i
     @user = User.find(user_id)
-    @posts_list = @user.recent_posts
+    @posts_list = @user.posts.all
   end
 
   def show
