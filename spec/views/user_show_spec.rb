44cf7b328c138f7a5b4d475344aafa90ff694419
rmrmrmr <ricardomartineza77@gmail.com>
2023-04-12 16:55:02 -0600

    Add mock data for users view tests

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'User show', type: :feature do
+  before :each do
+    @user1 = User.create(name: 'Tom', photo: 'https://randomuser.me/api/portraits/men/1.jpg', bio: 'Teacher from Mexico.', posts_counter: 4)
+
+    Post.create(id: 1, title: 'post 1', text: 'text 1', likes_counters: 0, comments_counter: 0, user_id: @user1.id)
+    Post.create(id: 2, title: 'post 2', text: 'text 2', likes_counters: 0, comments_counter: 0, user_id: @user1.id)
+    Post.create(id: 3, title: 'post 3', text: 'text 3', likes_counters: 0, comments_counter: 0, user_id: @user1.id)
+    Post.create(id: 4, title: 'post 4', text: 'text 4', likes_counters: 0, comments_counter: 0, user_id: @user1.id)
+  end
+
   user_path = '/users/1'
 
   scenario 'show user picture' do
